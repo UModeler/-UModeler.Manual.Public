07c6ed575ac50e3d8744ce04762c7998f73077c3
Tripolygon Service <service@tripolygon.com>
2024-02-26 10:12:49 +0000

    Deploy website - based on 6aa45fd5930bd2e5b1e423ecf80aa7e793227f9c

--- ko/assets/js/814f3328.01a56bf8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunku_modeler_manual=self.webpackChunku_modeler_manual||[]).push([[2535],{45641:e=>{e.exports=JSON.parse('{"title":"All posts","items":[{"title":"UModeler X Latest Update Introduction - January, February 2024","permalink":"/ko/blog/Feb-2024-Latest-Update"},{"title":"Starting the UModeler blog","permalink":"/ko/blog/hello-umodeler"}]}')}}]);
